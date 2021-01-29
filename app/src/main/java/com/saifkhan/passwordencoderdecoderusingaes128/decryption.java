4e618be809aa8c1e310742a79f45c6f5bb5d9a46
Saif Ahammod Khan <saif.ahammod@northsouth.edu>
2021-01-29 21:03:45 +0600

    Front End Complete

---
+++ app/src/main/java/com/saifkhan/passwordencoderdecoderusingaes128/decryption.java
@@ -0,0 +1,14 @@
+package com.saifkhan.passwordencoderdecoderusingaes128;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class decryption extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_decryption);
+    }
+}
